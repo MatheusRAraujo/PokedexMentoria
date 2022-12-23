17b203ef7951995aa3147db2645684c4b13f75dc
Matheus Rodrigues Araujo <mraraujo@uolinc.com>
2022-12-23 10:45:09 -0300

    Cria projeto do XCode

---
+++ PokedexMentoria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokedexMentoria
+//
+//  Created by Matheus Rodrigues Araujo on 23/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
